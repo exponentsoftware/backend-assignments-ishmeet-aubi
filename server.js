023a47550db992270f391ca761199630965db010
ishmeet-aubi <aubi.ishmeet@gmail.com>
2021-09-08 08:48:04 +0530

    Backend Assignments

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
-const todoRouter = require('./routes/todo');
-const categoryRouter = require('./routes/category')
-const querryRouter = require('./routes/additionalRoute')
-const userRouter = require('./routes/user')
-const ratingRouter = require('./routes/rating')
+const todoRouter = require("./routes/todo");
+const userRouter = require("./routes/user");
 const port = 3000;
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/todo',{
-    useNewUrlParser:true,
-    useUnifiedTopology : true
-})
-mongoose.connection.once('open', () =>{
-    console.log('connected with database')
+mongoose.connect("mongodb://localhost:27017/todo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected with database");
 });
 
 //routes
-app.use('/todo', todoRouter);
-app.use('/category', categoryRouter);
-app.use('/additional', querryRouter)
-app.use('/user', userRouter);
-app.use('/rating', ratingRouter);
-app.listen(port, () =>{
-    console.log(`server is running on port ${port}`)
-})
+app.use("/todo", todoRouter);
+app.use("/user", userRouter);
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
